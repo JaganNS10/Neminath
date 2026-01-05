de50693478e6fe875d57d9b7e442ad59b26d82b7
JaganNS10 <ns.jagansenthilkumar@gmail.com>
2026-01-05 19:16:12 +0530

    Latest commit

--- project/settings.py
+++ project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$l5p3u$yr(7!fjxm$ewl@uaop!ni!006fiu%_gyg^vl497c=p6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
