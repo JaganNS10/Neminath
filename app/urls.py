21714f49869d3ea0ca6cad3f1f26efeea3ede1c9
Jagan N.S <146404928+JaganNS10@users.noreply.github.com>
2026-01-13 13:34:13 +0530

    updated

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('home/',views.home,name='home'),
+    path('',views.home,name='home'),
     path('about/',views.about,name='about'),
     path('career/',views.career,name='career'),
     path('contact/',views.contact,name='contact'),
